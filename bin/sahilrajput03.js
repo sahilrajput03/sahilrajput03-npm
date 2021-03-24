b89bd9896f626058163f4bff960a9f5a48758cbc
Sahil Rajput <sahilrajput03@gmail.com>
2021-03-24 16:14:53 +0530

    ok, cool! ~ sahil

---
+++ bin/sahilrajput03.js
@@ -0,0 +1,13 @@
+#!/bin/node
+let text = ` ---
+---
+
+
+Hello, from sahil!
+
+I like to test and explore new techonlogies...!!
+
+Find me on twitter @freakstarrocks
+Find me on telegram @sahilrajput03
+Find me on dicord sahilrajput03#4631`
+console.log(text)
