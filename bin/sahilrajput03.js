e7a1beb1b7034e56cc0d4d6c2c960791b249a030
Sahil Rajput <sahilrajput03@gmail.com>
2021-05-18 18:55:17 +0530

    Update profile info. ~ sahil

--- bin/sahilrajput03.js
+++ bin/sahilrajput03.js
@@ -1,13 +1,15 @@
 #!/bin/node
-let text = ` ---
----
-
-
-Hello, from sahil!
-
+let text = `
+Hello, this is Sahil Rajput 🤹︎ 
+I am a fullstack developer🔥︎
 I like to test and explore new techonlogies...!!
 
-Find me on twitter @freakstarrocks
-Find me on telegram @sahilrajput03
-Find me on dicord sahilrajput03#4631`
+
+Twitter - https://twitter.com/freakstarrocks
+Telegram - @sahilrajput03
+Dicord - sahilrajput03#4631
+
+Join my server on discord and belive its going to be a 🤠︎ journey there as well.
+Discord server : https://discord.com/invite/Ahnc32dxzd
+`
 console.log(text)
