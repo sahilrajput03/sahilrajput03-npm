4085a00c8587a78b74fe38bcdc21475e758030f4
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-13 15:14:25 +0530

    update

--- sahilrajput03.js
+++ sahilrajput03.js
@@ -17,15 +17,19 @@ Hello, this is Sahil Rajput.
 I am a fullstack developer.
 I like to test and explore new techonlogies...!!
 
-Twitter   ~ https://twitter.com/freakstarrocks
-Telegram  ~ @sahilrajput03
+Community ~ https://discord.com/invite/Ahnc32dxzd
 Dicord    ~ sahilrajput03#4631
 GitHub    ~ https://github.com/sahilrajput03
 LinkedIn  ~ https://www.linkedin.com/in/sahilrajput03/
-Website   ~ https://sahilrajput03.ml, https://sahilrajput.ml, https://jaadujinn.ml (https://jujn.ml) 
-Community ~ https://discord.com/invite/Ahnc32dxzd
+Notes     ~ https://sahilrajput03.ml
+Telegram  ~ @sahilrajput03
+Twitter   ~ https://twitter.com/freakstarrocks
+Website   ~ https://sahilrajput.ml, https://jaadujinn.ml (https://jujn.ml) 
 
-o Join my server on discord and I belive its going to be a wonderful journey there as well.`),
+o Join my server on discord and I belive its going to be a wonderful journey there as well.
+o I am active on telegram regularly.
+
+Thanks for reading. Have a nice day!`),
 }
 
 const text = `${data.name}
