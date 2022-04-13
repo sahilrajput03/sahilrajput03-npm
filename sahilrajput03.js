bcac6a03a1add2f9b9bf8798eada7ec4f6c5f2f0
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-13 15:21:55 +0530

    update

--- sahilrajput03.js
+++ sahilrajput03.js
@@ -4,15 +4,11 @@ import chalk from "chalk"
 import clear from "clear"
 import open from "open"
 import inquirer from "inquirer"
-// import prompt from 'prompt'
-
-// clear the terminal before showing the npx card
-// clear()
 
 const cyan = "#00AB9E"
-const data = {
-	name: chalk.bold.cyan("Sahil Rajput"),
-	npx: chalk.hex(cyan)(`
+const name = chalk.bold.cyan("Sahil Rajput")
+
+const long_text = chalk.hex(cyan)(`
 Hello, this is Sahil Rajput.
 I am a fullstack developer.
 I like to test and explore new techonlogies...!!
@@ -29,11 +25,10 @@ Website   ~ https://sahilrajput.ml, https://jaadujinn.ml (https://jujn.ml)
 o Join my server on discord and I belive its going to be a wonderful journey there as well.
 o I am active on telegram regularly.
 
-Thanks for reading. Have a nice day!`),
-}
+Thanks for reading. Have a nice day!`)
 
-const text = `${data.name}
-${data.npx}`
+const text = `${name}
+${long_text}`
 
 const me = boxen(text, {
 	margin: 1,
@@ -45,4 +40,3 @@ const me = boxen(text, {
 
 // Show the boxen
 console.log(me)
-// prompt(questions).then((answer) => answer.action())
